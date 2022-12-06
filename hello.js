eb9c42c9a5e420dff3e6141900a570993a30e90f
efburgosr <32855821+efburgosr@users.noreply.github.com>
2022-12-05 23:18:49 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,12 @@
-#!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello World! Node.js is working correctly.\n');
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo.');
+});
+
+server.listen(port, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
